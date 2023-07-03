7e3f9e7c0227a56f6a995a5b1dfa36ba5f1c19c9
eogasawara <eogasawara@eic.cefet-rj.br>
2023-07-03 11:00:43 -0300

    update

--- R/ts_conv1d.R
+++ R/ts_conv1d.R
@@ -1,11 +1,11 @@
-#'@title LSTM
+#'@title Conv1D
 #'@description Creates a time series prediction object that
-#' uses the LSTM.
+#' uses the Conv1D
 #' It wraps the pytorch library.
 #'@param preprocess normalization
 #'@param input_size input size for machine learning model
 #'@param epochs maximum number of epochs
-#'@return a `ts_lstm` object.
+#'@return a `ts_conv1d` object.
 #'@examples
 #'\dontrun{
 #'library(daltoolbox)
@@ -17,7 +17,7 @@
 #'io_train <- ts_projection(samp$train)
 #'io_test <- ts_projection(samp$test)
 #'
-#'model <- ts_lstm(ts_norm_gminmax(), input_size=4, epochs = 10000L)
+#'model <- ts_conv1d(ts_norm_gminmax(), input_size=4, epochs = 10000L)
 #'model <- fit(model, x=io_train$input, y=io_train$output)
 #'
 #'prediction <- predict(model, x=io_test$input[1,], steps_ahead=5)
@@ -30,37 +30,33 @@
 #'@import daltoolbox
 #'@import reticulate
 #'@export
-ts_lstm <- function(preprocess = NA, input_size = NA, epochs = 10000L) {
+ts_conv1d <- function(preprocess = NA, input_size = NA, epochs = 10000L) {
   obj <- ts_regsw(preprocess, input_size)
+  obj$channels <- 1
   obj$deep_debug <- FALSE
   obj$epochs <- epochs
-  class(obj) <- append("ts_lstm", class(obj))
-
+  class(obj) <- append("ts_conv1d", class(obj))
   return(obj)
 }
 
 #'@export
-do_fit.ts_lstm <- function(obj, x, y) {
-  reticulate::source_python(system.file("python", "ts_lstm.py", package = "daltoolboxext"))
-
+do_fit.ts_conv1d <- function(obj, x, y) {
   if (is.null(obj$model))
-    obj$model <- create_torch_lstm(obj$input_size, obj$input_size)
+    obj$model <- create_torch_conv1d(obj$channels, obj$input_size)
 
   df_train <- as.data.frame(x)
   df_train$t0 <- as.vector(y)
 
-  obj$model <- train_torch_lstm(obj$model, df_train, obj$epochs, 0.001, obj$deep_debug, obj$reproduce)
+  obj$model <- train_torch_conv1d(obj$model, df_train, obj$epochs, 0.001, obj$deep_debug, obj$reproduce)
 
   return(obj)
 }
 
 #'@export
-do_predict.ts_lstm <- function(obj, x) {
-  reticulate::source_python(system.file("python", "ts_lstm.py", package = "daltoolboxext"))
-
+do_predict.ts_conv1d <- function(obj, x) {
   X_values <- as.data.frame(x)
   X_values$t0 <- 0
-  prediction <- predict_torch_lstm(obj$model, X_values)
+  prediction <- predict_torch_conv1d(obj$model, X_values)
   prediction <- as.vector(prediction)
   return(prediction)
 }
