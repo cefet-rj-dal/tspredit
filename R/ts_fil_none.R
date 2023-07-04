edf9b47bbeda23b48022e72c5f8c5725fa22a026
eogasawara <eogasawara@eic.cefet-rj.br>
2023-07-04 20:17:35 -0300

    Update filter

---
+++ R/ts_fil_none.R
@@ -0,0 +1,30 @@
+#'@title no filter
+#'@description Does not make data filter
+#'@return a `ts_fil_none` object.
+#'@examples
+#'library(daltoolbox)
+#'data(sin_data)
+#'
+#'#convert to sliding windows
+#'xw <- ts_data(sin_data$y, 10)
+#'
+#'#no data filter
+#'augment <- ts_fil_none()
+#'augment <- fit(augment, xw)
+#'xa <- transform(augment, xw)
+#'ts_head(xa)
+#'@export
+ts_fil_none <- function() {
+  obj <- dal_transform()
+  class(obj) <- append("ts_fil_none", class(obj))
+  return(obj)
+}
+
+#'@export
+transform.ts_fil_none <- function(obj, data, ...) {
+  result <- data
+  idx <- c(1:nrow(result))
+  attr(result, "idx") <- idx
+  return(result)
+}
+
